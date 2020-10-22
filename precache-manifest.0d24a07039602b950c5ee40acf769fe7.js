682d639fca0dd14d4e06076b5afe1463a2aa89cd
DevidJoshua <28devid@gmail.com>
2020-10-22 18:11:55 +0700

    renew

---
+++ precache-manifest.0d24a07039602b950c5ee40acf769fe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac07777acdc32c2ce4e05f517a98d53",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "13381f4740268439da9f",
+    "url": "/build/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "40285bbc64aa063e7e58",
+    "url": "/build/static/js/2.baf7403f.chunk.js"
+  },
+  {
+    "revision": "78fd0f62364854e39c53711d027f73a3",
+    "url": "/build/static/js/2.baf7403f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13381f4740268439da9f",
+    "url": "/build/static/js/main.40cf818c.chunk.js"
+  },
+  {
+    "revision": "ad42ab415160150cf5f5",
+    "url": "/build/static/js/runtime-main.ace65150.js"
+  }
+]);
